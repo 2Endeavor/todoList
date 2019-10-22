bedbc19d1d8ebdc80754b7d60d56c2d6849929fd
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-10-21 18:34:51 -0700

    added buttons and option to remove task

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,17 @@
 import React, {useState} from 'react';
 import'./App.css'
 
-function Todo({todo, index}) {
+function Todo({todo, index,completeTodo,removeTodo}) {
   return(
-    <div className="todo">
+    <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} className="todo">
       {todo.text}
+      <div>
+        <button onClick={()=>completeTodo(index)}>Complete</button>
+        <button onClick={()=>removeTodo(index)}>X</button>
+        
+
+      
+      </div>
     </div>
   )
 }
@@ -56,7 +63,17 @@ function App(){
  const addTodo = text =>{
    const newTodos = [...todos, {text}];
    setTodos(newTodos);
- }
+ };
+  const completeTodo = index =>{
+    const newTodos = [...todos];
+    newTodos[index].isCompleted=true;
+    setTodos(newTodos);
+  }
+  const removeTodo=index=>{
+    const newTodos = [...todos];
+    newTodos.splice(index,1);
+    setTodos(newTodos)
+  }
  return (
    <div className="app">
 
@@ -64,7 +81,13 @@ function App(){
 
   {todos.map((todo, index)=>(
   
-    <Todo key={index} index={index} todo={todo}/>
+    <Todo 
+    key={index} 
+    index={index} 
+    todo={todo} 
+    completeTodo={completeTodo}
+    removeTodo={removeTodo}
+    />
   ))}
   <TodoForm addTodo={addTodo}/>
       </div>
